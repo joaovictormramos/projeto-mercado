c15dd9ba2cfe739eea3dd45440dc0370e9cb4cb8
joaovictormramos <molinajoao10@gmail.com>
2024-06-11 01:25:48 -0400

    botão retornar

--- app/views/usuario/detalhes.php
+++ app/views/usuario/detalhes.php
@@ -1,5 +1,16 @@
 <?php
 $this->layout('master', ['title' => 'Detalhes lista']);
+$lista_nome = $listaDesc[0]->lista_nome;
 ?>
 
-<h1><?php var_dump($listaDesc); ?></h1>
+<h2><?php echo $lista_nome; ?></h2>
+
+<?php
+
+foreach ($listaDesc as $produto) {?>
+    <p><?php echo $produto->produto_produto . ' ' . $produto->marca_nome . ' '
+    . str_replace('.', ',', (string) $produto->produto_medida) . $produto->produto_unidademedida; ?></p>
+<?php
+}
+?>
+<a href="../minhaslistas">Retornar</a>
