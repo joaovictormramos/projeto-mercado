2bf7c1da15e867e59c7dc988bf1447ad376cb40c
joaovictormramos <molinajoao10@gmail.com>
2024-07-22 16:23:36 -0400

    fix dbconfig

--- app/config/Connect.php
+++ app/config/Connect.php
@@ -6,20 +6,22 @@ define('HOST', 'postgres.railway.internal');
 define('DATABASENAME', 'railway');
 define('USER', 'postgres');
 define('PASSWORD', 'wkWPBnOQFXrthmBErLgmdxDNmssArJbr');
+define('PORT', '5432');
 
 class Connect
 {
     private static $connection;
-
+    
     public function __construct()
     {
     }
 
     public static function connectDatabase()
     {
+        $dsn = 'pgsql:host=' . HOST . ';port=' . PORT . ';dbname=' . DATABASENAME;
         if (!isset(self::$connection)) {
             try {
-                self::$connection = new \PDO('pgsql:host=' . HOST . ', dbname=' . DATABASENAME, USER, PASSWORD);
+                self::$connection = new \PDO($dsn, USER, PASSWORD);
             } catch (\PDOException $e) {
                 echo "Connection failed: " . $e->getMessage();
                 die();
